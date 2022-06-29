383e6467b1a1092f3e04fa0d5591c2b06b5d45a8
MOHAMMAD HASSNAIN <91183477+hassni12@users.noreply.github.com>
2022-06-29 20:27:16 +0500

    dsa

--- dsa.js
+++ dsa.js
@@ -1,93 +1,93 @@
- //     function same(arr1,arr2){
-      //         if (arr1.length!==arr2.length){
-      //             return false
-      //         }
-      //         for (let i=0; i<arr1.length; i++){
-      //             let curentIndex=arr2.indexOf(arr1[i]**2)
+//     function same(arr1,arr2){
+//         if (arr1.length!==arr2.length){
+//             return false
+//         }
+//         for (let i=0; i<arr1.length; i++){
+//             let curentIndex=arr2.indexOf(arr1[i]**2)
 
-      //             if (curentIndex==-1){
-      //                 return false
-      //             }
-      //             arr2.splice(curentIndex,1)
-      //         }
-      //         return true
+//             if (curentIndex==-1){
+//                 return false
+//             }
+//             arr2.splice(curentIndex,1)
+//         }
+//         return true
 
-      //     }
-      //   console.log(same([3,2,1],[9,4,1]))
-      // let arr1 ="hassnain"
-      // console.log(arr1.indexOf("n")
+//     }
+//   console.log(same([3,2,1],[9,4,1]))
+// let arr1 ="hassnain"
+// console.log(arr1.indexOf("n")
 
-      // console.log(result)
-      // "hassnain"
-      //       function count(str) {
-      // let result = {};
-      //         for (let i = 0; i < str.length; i++) {
+// console.log(result)
+// "hassnain"
+//       function count(str) {
+// let result = {};
+//         for (let i = 0; i < str.length; i++) {
 
-      //           if (result[str[i]] > 0) {
-      //             result[str[i]]++;
-      //           } else {
-      //             result[str[i]] = 1;
+//           if (result[str[i]] > 0) {
+//             result[str[i]]++;
+//           } else {
+//             result[str[i]] = 1;
 
-      //           }
+//           }
 
-      //         }
-      //         return result;
-      //       }
-      //       console.log(count("hhhjk"));
-      //   let count1 = {};
-      //   arr1 = "hassnain";
-      // function same(arr1, arr2) {
-      //     if (arr1.lenght!==arr2.lenght)
-      //     {
-      //         return new Error("arry are not equal in size")
-      //     }
-      //   let count1={};
-      //   let count2={};
+//         }
+//         return result;
+//       }
+//       console.log(count("hhhjk"));
+//   let count1 = {};
+//   arr1 = "hassnain";
+// function same(arr1, arr2) {
+//     if (arr1.lenght!==arr2.lenght)
+//     {
+//         return new Error("arry are not equal in size")
+//     }
+//   let count1={};
+//   let count2={};
 
-      //   for (let val of arr1) {
-      //     count1[val] = (count1[val] || 0) + 1;
-      //   }
-      //       console.log(count1);
-      //   for (let val of arr2) {
-      //     count1[val] = (count1[val] || 0) + 1;
-      //   }
-      //   for (let key in count1){
-      //       if (!(key**2 in count2) ){
-      //           return false
-      //       }
+//   for (let val of arr1) {
+//     count1[val] = (count1[val] || 0) + 1;
+//   }
+//       console.log(count1);
+//   for (let val of arr2) {
+//     count1[val] = (count1[val] || 0) + 1;
+//   }
+//   for (let key in count1){
+//       if (!(key**2 in count2) ){
+//           return false
+//       }
 
-      //   }
-      //   return true
-      //   // for ()
-      // }
-      // console.log(same([4,4,2],[16,16,4]))
+//   }
+//   return true
+//   // for ()
+// }
+// console.log(same([4,4,2],[16,16,4]))
 
-      // arr1="hassnain"
-      // let count2={name:"hassnain"}
-      //     for (let val in count2) {
-      //     //   count1[val] = (count1[val] || 0) + 1;
-      //     console.log(val)
-      // }
+// arr1="hassnain"
+// let count2={name:"hassnain"}
+//     for (let val in count2) {
+//     //   count1[val] = (count1[val] || 0) + 1;
+//     console.log(val)
+// }
 
-    //   function reversmatch(str,str1){
-    //     if (str.length!==str1.length){
-    //       return false
-    //     }
-    //     let result="";
-    //   for (let i = str.length-1; i >=0;  i--) {
-    //     result+=str[i]
-      
-    //   }
-    //   if (result===str1){
-    //       return true
-    //     }else{
-    //       return false
-    //     }
-     
-    //       console.log(result)
+//   function reversmatch(str,str1){
+//     if (str.length!==str1.length){
+//       return false
+//     }
+//     let result="";
+//   for (let i = str.length-1; i >=0;  i--) {
+//     result+=str[i]
 
-    // }
-    // console.log(anagram("hass","ssah"))
+//   }
+//   if (result===str1){
+//       return true
+//     }else{
+//       return false
+//     }
+
+//       console.log(result)
+
+// }
+// console.log(anagram("hass","ssah"))
 // function(){
 
 // }
@@ -103,35 +103,33 @@
 // }else{
 //   result[str1[i]]-=1
 // }
-  
+
 //   }
-// return true 
+// return true
 // }
 // [-3,-2,-1,0,1,2,3]
 // function pointer(arr){
 //   for(let i=0; i<arr.length; i++){
 //     for(let j=i+1; j<arr.length; j++){
-      
-      
+
 //       if ((arr[i]+arr[j])===0){
 //         return [arr[i],arr[j]]
 //       }
-//     } 
+//     }
 //   }
 // }
 
-
 // function countuniquevaluesinarry(arr){
-//   let i=0 
+//   let i=0
 //   for (let j=1; j<arr.length; j++){
 //     if (arr[i]!==arr[j]){
 //       i++
-//      console.log(arr[i]=arr[j]) // 2,3,4,5 
+//      console.log(arr[i]=arr[j]) // 2,3,4,5
 //     }
 //   }
 //    return i+1
 // }
-// console.log(countuniquevaluesinarry([1,1,1,2,2,2,3,4,5])) 
+// console.log(countuniquevaluesinarry([1,1,1,2,2,2,3,4,5]))
 
 // arr=[1,1,2,3,4,4,5]
 // let result =""
@@ -140,7 +138,6 @@
 // // console.log(i)
 // }
 // console.log(result)
-
 
 //     function same(arr1,arr2){
 //         if (arr1.length!==arr2.length){
@@ -612,7 +609,6 @@
 
 // function mergeSort(firstArray, secondArray) {
 
-
 //   let result = [];
 //   let i = 0;
 //   let j = 0;
@@ -623,7 +619,6 @@
 //     } else  {
 //       result.push(secondArray[j]);
 //       j++;
-
 
 //     }
 //   }
@@ -636,22 +631,40 @@
 //     j++
 //   }
 // return result
-// //  console.log(result) 
+// //  console.log(result)
 // }
 // console.log(mergeSort([1, 10, 50], [2, 14, 99, 100]));
 
+function pivot(arr, start = 0, end = arr.length-1) {
+  let pivot = arr[start];
+  let swapIndex = start;
+  for (let i = start+1; i <=end; i++) {
+    if (pivot > arr[i]) {
+      swapIndex++;
+      temp = arr[i];
+      arr[i] = arr[swapIndex];
+      arr[swapIndex] = temp;
+    }
+  }
+  temp = arr[start];
+  arr[start] = arr[swapIndex];
+  arr[swapIndex] = temp;
 
+  return swapIndex;
+}
+function quickSort(arr, left = 0, right = arr.lenght-1) {
+ 
+  if (left < right) {
+   
+    
+    let pivotIndex = pivot(arr, left, right);
+     //left 
+    quickSort(arr, left, pivotIndex - 1);
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(quickSort([4, 8, 2, 1, 5, 6, 3]));
+// [3,2,1,4,5,7,6,8]
