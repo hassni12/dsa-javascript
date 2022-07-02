0201ae43147ee8bc11f7c4f66fe5ec960a5e0706
MOHAMMAD HASSNAIN <91183477+hassni12@users.noreply.github.com>
2022-07-02 18:54:56 +0500

    linked list

--- dsa.js
+++ dsa.js
@@ -715,33 +715,67 @@
 
 //   console.log(redixSort([23,345,5467,12,2345,9852]));
 
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
-}
-class SinglyLinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
-  push(val) {
-    let newNode = new Node(val);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = this.head;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-    this.length++;
-    return this;
-  }
-}
-let list = new SinglyLinkedList();
-console.log(list.push("hi"));
-console.log(list.push("hello"));
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+// class SinglyLinkedList {
+//   constructor() {
+//     this.head = null;
+//     this.tail = null;
+//     this.length = 0;
+//   }
+//   push(val) {
+//     let newNode = new Node(val);
+//     if (!this.head) {
+//       this.head = newNode;
+//       this.tail = this.head;
+//     } else {
+//       this.tail.next = newNode;
+//       // console.log(this.tail.next,"thid");
+//       this.tail = newNode;
+//     }
+//     this.length++;
+//     return this;
+//   }
+//   pop() {
+//     if (!this.head) return undefined;
+//     let current = this.head;
+//     let newTail = current;
+//     while (current.next) {
+//       newTail = current;
+//       current = current.next;
+//     }
+
+//     this.tail = newTail;
+//     this.tail.next = null;
+//     this.length--;
+//     if (this.length===0){
+//       this.head = null;
+//       this.tail=null
+//     } 
+//     return current;
+
+//     // console.log(newTail.val)
+//     // console.log(current.val)
+//   }
+// }
+// let list = new SinglyLinkedList();
 // console.log(list.push("hassnain"));
-// console.log(list.push("qureshi"));
+// console.log(list.push("hi"));
+// console.log(list.push("hello"));
+// // console.log(list.pop(),"this")
+
+// // console.log(list.push("qureshi"));
+
+
+
+class Node{
+  constructor(val,next=null){
+    this.val=val;
+    this.next=next;
+
+  }
+}
